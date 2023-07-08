99ac4cc4aea2af9bd9a249563803bc10c9ca1e9d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-08 22:35:10 +0200

    Update image-resizer.py

--- image-resizer.py
+++ image-resizer.py
@@ -15,3 +15,4 @@ def resize_image(input_path, output_path, new_width, new_height):
 # Usage
 input_path = "picture.png"  # Path to the input image file
 output_path = "output.png"  # Path to save the resized image
+new_width = 800  # Desired width for the resized image
