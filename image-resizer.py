6d6f259c97e0bc37c4d9a4afb529cd4f71b59c0c
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-08 22:31:55 +0200

    Update image-resizer.py

--- image-resizer.py
+++ image-resizer.py
@@ -5,3 +5,4 @@ def resize_image(input_path, output_path, new_width, new_height):
     image = Image.open(input_path)
 
     # Resize the image
+    resized_image = image.resize((new_width, new_height))
